f1809225ca53f0c9381597c458b2d0bcf6dc5367
taejung2183 <taejung2183@naver.com>
2021-03-19 22:26:17 +0900

    Add supplementary view for badge on a tutorial

---
+++ RayWenderlichLibrary/RayWenderlichLibrary/Views/BadgeSupplementaryView.swift
@@ -0,0 +1,9 @@
+//
+//  BadgeSupplementaryView.swift
+//  RayWenderlichLibrary
+//
+//  Created by atj on 2021/03/19.
+//  Copyright © 2021 Ray Wenderlich. All rights reserved.
+//
+
+import Foundation
