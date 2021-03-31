32638c8812a76a79cd35fe49243e45ac30fbd81e
taejung2183 <taejung2183@naver.com>
2021-03-31 15:55:16 +0900

    Add spring and transition animations

--- PackingList_UIKit/PackingList/ViewController.swift
+++ PackingList_UIKit/PackingList/ViewController.swift
@@ -44,6 +44,10 @@ final class ViewController: UIViewController {
     
     @IBOutlet private var menuHeightConstraint: NSLayoutConstraint!
     @IBOutlet private var menuButtonTrailingConstraint: NSLayoutConstraint!
+    
+    @IBOutlet private var titleCenterYClosedConstraint: NSLayoutConstraint!
+    // Uncheck the installed in the attribute inspector so that this constraint is not already activated when the app is started.
+    @IBOutlet private var titleCenterYOpenConstraint: NSLayoutConstraint!
 
     //MARK:- Variables
     
@@ -72,17 +76,47 @@ private extension ViewController {
         // Calling layoutIfNeeded() right after changing text label makes it chatch up the text label's boundary change without animation.
         view.layoutIfNeeded()
         
+        // Adjust the priority differently in the attribute inspector so that there's no conflict when both constraints are active before the second one deactivates.
+        titleCenterYClosedConstraint.isActive = !menuIsOpen
+        titleCenterYOpenConstraint.isActive = menuIsOpen
+
+        let constraints = titleLabel.superview!.constraints
+        constraints.first {
+            // Comparing object references.
+            $0.firstItem === titleLabel && $0.firstAttribute == .centerX
+        }?.constant = menuIsOpen ? -100 : 0
+        
+        /*
+        You can code a constraints just as below or set an IBOutlet.
+        
+        // If you deactivate a constraint and don't hold the reference to it, it'll be deleted from memory the next time the layout is updated.
+        constraints.first { $0.identifier == "Title Center Y" }!.isActive = false
+        // Setting up a new constraint.
+        let constraint = NSLayoutConstraint(item: titleLabel!, attribute: .centerY, relatedBy: .equal, toItem: titleLabel.superview, attribute: .centerY, multiplier: menuIsOpen ? 2 / 3 : 1, constant: 0)
+        constraint.identifier = "Title Center Y"
+        constraint.priority = .defaultHigh
+        constraint.isActive = true
+        */
+
         // One call for layoutIfNeeded() inside of the animate method will animate all the constraint changes waiting for an update.
         menuHeightConstraint.constant = menuIsOpen ? 200 : 80
         menuButtonTrailingConstraint.constant = menuIsOpen ? 16 : 8
         
-        UIView.animate(withDuration: 1 / 3, delay: 0, options: .curveEaseIn, animations: {
-            self.menuButton.transform = .init(rotationAngle: self.menuIsOpen ? .pi / 4 : 0)
-            self.view.layoutIfNeeded()
-        })
+        UIView.animate(
+            withDuration: 1, delay: 0,
+            usingSpringWithDamping: 0.4, initialSpringVelocity: 10,
+            // You can't mix time easing curve with spring animaitons.
+            // Be careful with using allowUserInteraction option because it can cause a performance hit.
+            options: .allowUserInteraction,
+            animations: {
+                self.menuButton.transform = .init(rotationAngle: self.menuIsOpen ? .pi / 4 : 0)
+                self.view.layoutIfNeeded()
+            }
+        )
     }
     
     func showItem(_ item: Item) {
+        // Setting image view for previewing imoji.
         let imageView = UIImageView(item: item)
         imageView.backgroundColor = .init(white: 0, alpha: 0.5)
         imageView.layer.cornerRadius = 5
@@ -90,39 +124,81 @@ private extension ViewController {
         imageView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(imageView)
         
-        let bottomConstraint = imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: imageView.frame.height)
-        let widthConstraint = imageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1 / 3, constant: -50)
+        // If you want to use transition, the views you want to effect should be wrapped in a separate container view or superview.
+        let containerView = UIView(frame: imageView.frame)
+        view.addSubview(containerView)
+        containerView.addSubview(imageView)
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let bottomConstraint = containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: containerView.frame.height)
+        let widthConstraint = containerView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1 / 3, constant: -50)
         
         NSLayoutConstraint.activate([
             bottomConstraint,
             widthConstraint,
-            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            containerView.heightAnchor.constraint(equalTo: containerView.widthAnchor),
+
+            imageView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+            imageView.widthAnchor.constraint(equalTo: containerView.widthAnchor),
             // Constraint that keep the image view in square aspect ratio.
             imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor)
         ])
         // Call layoutIfNeeded() to force auto layout to update so that the layout constraints you defined above can be applied.
         view.layoutIfNeeded()
         
-        UIView.animate(withDuration: 0.8, animations: {
+        UIView.animate(
+            withDuration: 0.8, delay: 0,
+            usingSpringWithDamping: 0.6, initialSpringVelocity: 10,
+            // You can't mix time easing curve with spring animaitons.
+            options: [],
+            animations: {
+                bottomConstraint.constant = imageView.frame.height * -2
+                widthConstraint.constant = 0
+                self.view.layoutIfNeeded()
+            }
+        )
+        
+        // Pop up the imoji.
+        /*UIView.animate(withDuration: 0.8, animations: {
             bottomConstraint.constant = imageView.frame.height * -2
             widthConstraint.constant = 0
             self.view.layoutIfNeeded()
-        })
+        })*/
         
-        UIView.animate(withDuration: 2 / 3,delay: 1.2, animations: {
-            bottomConstraint.constant = imageView.frame.height
-            widthConstraint.constant = -50
-            self.view.layoutIfNeeded()
-        },
-        completion: {
-            // You can define something that you want to execute after the animation executed.
-            // The view hierarchy only holds the image view when it's showing.
-            _ in imageView.removeFromSuperview()
+        // Screen out the poped up imoji.
+        /*UIView.animate(
+            withDuration: 2 / 3,delay: 1.2,
+            animations: {
+                bottomConstraint.constant = imageView.frame.height
+                widthConstraint.constant = -50
+                self.view.layoutIfNeeded()
+            },
+            completion: {
+                // You can define something that you want to execute after the animation executed.
+                // In this way, the view hierarchy only holds the image view when it's showing.
+                _ in imageView.removeFromSuperview()
+            }
+        )*/
+        delay(seconds: 1, execute: {
+            UIView.transition(with: containerView, duration: 1, options: .transitionFlipFromBottom, animations: imageView.removeFromSuperview, completion: { _ in containerView.removeFromSuperview() })
         })
     }
     
     func transitionCloseMenu() {
         delay(seconds: 0.35, execute: toggleMenu)
+        
+        // Get a hold of the menu and store a reference in a local constant to use as a container view.
+        let titleBar = slider.superview!
+        UIView.transition(
+            with: titleBar,
+            duration: 0.5,
+            options: .transitionFlipFromBottom,
+            animations: { self.slider.isHidden = true },
+            // We don't want to permanently hide the slider.
+            completion: { _ in self.slider.isHidden = false}
+        )
     }
 }
 
