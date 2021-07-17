2ab14ebf2e04639c74885641e971834592bb282d
taejung2183 <taejung2183@naver.com>
2021-07-17 15:41:49 +0900

    Test convert method with full coverage

--- Numero_TDD_Tutorial/Numero/NumeroTests/ConverterTests.swift
+++ Numero_TDD_Tutorial/Numero/NumeroTests/ConverterTests.swift
@@ -47,5 +47,33 @@ class ConverterTests: XCTestCase {
 		let result = converter.convert(9)
 		XCTAssertEqual(result, "IX", "Conversion for 9 is incorrect")
 	}
-
+	func testConverstionForZero() {
+		let result = converter.convert(0)
+		// Zero isn't represented in Roman numerals.
+		XCTAssertEqual(result, "", "Conversion for 0 is incorrect")
+	}
+	func testConverstionFor3999() {
+		let result = converter.convert(3999)
+		XCTAssertEqual(result, "MMMCMXCIX", "Conversion for 3999 is incorrect")
+	}
+	func testConverstionFor40() {
+		let result = converter.convert(40)
+		XCTAssertEqual(result, "XL", "Conversion for 40 is incorrect")
+	}
+	func testConverstionFor50() {
+		let result = converter.convert(50)
+		XCTAssertEqual(result, "L", "Conversion for 50 is incorrect")
+	}
+	func testConverstionFor100() {
+		let result = converter.convert(100)
+		XCTAssertEqual(result, "C", "Conversion for 100 is incorrect")
+	}
+	func testConverstionFor400() {
+		let result = converter.convert(400)
+		XCTAssertEqual(result, "CD", "Conversion for 400 is incorrect")
+	}
+	func testConverstionFor500() {
+		let result = converter.convert(500)
+		XCTAssertEqual(result, "D", "Conversion for 500 is incorrect")
+	}
 }
