c253a4ee800370281da479a44e2a035a5ddd337c
taejung2183 <taejung2183@naver.com>
2021-07-17 15:43:39 +0900

    Complete convert function

--- Numero_TDD_Tutorial/Numero/Numero/Converter.swift
+++ Numero_TDD_Tutorial/Numero/Numero/Converter.swift
@@ -32,7 +32,15 @@ class Converter {
 		var result = ""
 		
 		let numberSymbols: [(number: Int, symbol: String)] =
-			[(10, "X"),
+			[(1000, "M"),
+			 (900, "CM"),
+			 (500, "D"),
+			 (400, "CD"),
+			 (100, "C"),
+			 (90, "XC"),
+			 (50, "L"),
+			 (40, "XL"),
+			 (10, "X"),
 			 (9, "IX"),
 			 (5, "V"),
 			 (4, "IV"),
