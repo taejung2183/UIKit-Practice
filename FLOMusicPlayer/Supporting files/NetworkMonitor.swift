fa95268b6950c67d2176e6f43107309e8e77b16b
taejung2183 <taejung2183@naver.com>
2021-07-28 18:54:35 +0900

    Add network monitor for XCTSkip

---
+++ FLOMusicPlayer/Supporting files/NetworkMonitor.swift
@@ -0,0 +1,31 @@
+//
+//  NetworkMonitor.swift
+//  FLOMusicPlayer
+//
+//  Created by atj on 2021/07/28.
+//
+import Network
+
+class NetworkMonitor {
+	static let shared = NetworkMonitor()
+	var isReachable: Bool { status == .satisfied }
+
+	private let monitor = NWPathMonitor()
+	private var status = NWPath.Status.requiresConnection
+
+	private init() {
+		startMonitoring()
+	}
+
+	func startMonitoring() {
+		monitor.pathUpdateHandler = { [weak self] path in
+			self?.status = path.status
+		}
+		let queue = DispatchQueue(label: "NetworkMonitor")
+		monitor.start(queue: queue)
+	}
+
+	func stopMonitoring() {
+		monitor.cancel()
+	}
+}
