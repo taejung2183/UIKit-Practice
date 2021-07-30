03c51d2af989ef5e684bbf8f3181057bc57aa4c3
taejung2183 <taejung2183@naver.com>
2021-07-30 23:32:56 +0900

    Add mocked URLSession with protocol

--- FLOMusicPlayer/Supporting files/MockedURLSession.swift
+++ FLOMusicPlayer/Supporting files/MockedURLSession.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+protocol URLSessionProtocol {
+	func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol
+}
+//extension URLSession: URLSessionProtocol {}
+
+protocol URLSessionDataTaskProtocol {
+	func resume()
+}
+//extension URLSessionDataTask: URLSessionDataTaskProtocol {}
+
+class URLSessionDataTaskMock: URLSessionDataTaskProtocol {
+	private let closure: () -> Void
+	init(closure: @escaping () -> Void) {
+		self.closure = closure
+	}
+	func resume() {
+		closure()
+	}
+}
+
+class URLSessionMock: URLSessionProtocol {
+	typealias CompletionHandler = (Data?, URLResponse?, Error?) -> Void
+	var data: Data?
+	var error: Error?
+	func dataTask(
+		with url: URL,
+		completionHandler: @escaping CompletionHandler
+	) -> URLSessionDataTaskProtocol {
+		let data = self.data
+		let error = self.error
+		return URLSessionDataTaskMock {
+			completionHandler(data, nil, error)
+		}
+	}
+}
