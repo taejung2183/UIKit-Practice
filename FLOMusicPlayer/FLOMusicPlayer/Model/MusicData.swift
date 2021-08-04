9a3cadd4a7ab3c57e3e30afab8ae081eff1d14ac
taejung2183 <taejung2183@naver.com>
2021-08-04 20:26:47 +0900

    Add completion handler call  at the getMusic method to give music data to the test function

--- FLOMusicPlayer/FLOMusicPlayer/Model/MusicData.swift
+++ FLOMusicPlayer/FLOMusicPlayer/Model/MusicData.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Data: Codable {
+struct Music: Codable {
 	let singer: String
 	let album: String
 	let title: String
@@ -18,20 +18,18 @@ struct Data: Codable {
 }
 
 class MusicData {
-	var data: Data
-	
-	init (data: Data) {
-		self.data = data
-	}
-	
-	func downloadData<T: URLSessionProtocol>(_ session: T, completionBlock: @escaping (Result<Data, Error>) -> Void) {
-		if let url = URL(string: "https://grepp-programmers-challenges.s3.ap-northeast-2.amazonaws.com/2020-flo/song.json") {
-			let task = session.dataTask(with: url, completionHandler: {data, urlresponse, error in
-				if let data = data {
-					completionBlock(.success(data))
-				}
-			})
-			task.resume()
+	var session: URLSessionProtocol!
+	func getMusic(from urlStr: String, completion: @escaping (Music?, Error?) -> Void) {
+		guard let url = URL(string: urlStr)
+		else { fatalError() }
+		
+		let dataTask = session.dataTask(with: url) { data, response, error in
+			guard let data = data else { return }
+			let music = try! JSONDecoder().decode(Music.self, from: data)
+			completion(music, nil)
 		}
+		dataTask.resume()
 	}
 }
+
+
