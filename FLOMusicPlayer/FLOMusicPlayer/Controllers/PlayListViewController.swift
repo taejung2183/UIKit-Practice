fefa1fa6e617fe678064be9f31f36641eb7b4bf4
taejung2183 <taejung2183@naver.com>
2021-08-09 16:29:47 +0900

    Create table view controller for play list

--- FLOMusicPlayer/FLOMusicPlayer/Controllers/PlayListViewController.swift
+++ FLOMusicPlayer/FLOMusicPlayer/Controllers/PlayListViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PlayListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+	let tableView = UITableView()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		view.addSubview(tableView)
+		tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+		tableView.delegate = self
+		tableView.dataSource = self
 	}
 
-
+	override func viewDidLayoutSubviews() {
+		super.viewDidLayoutSubviews()
+		tableView.frame = view.bounds
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		10
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+		
+		cell.textLabel?.text = "Cell \(indexPath.row + 1)"
+		return cell
+	}
 }
 
